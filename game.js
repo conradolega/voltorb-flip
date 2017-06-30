bff42dd2204f382003b21f02ca6f39d3d3cf3b6c
Conrad Olega <conradolega@gmail.com>
2017-06-30 18:52:33 +0800

    Show card values

--- game.js
+++ game.js
@@ -3,3 +3,11 @@ const values = [0, 1, 2, 3];
 const board = Array(5).fill().map((el) => {
   return Array(5).fill().map((el) => values[Math.floor(4 * Math.random())]);
 });
+
+const rows = document.querySelectorAll('.grid .row');
+rows.forEach((row, x) => {
+  const cards = row.querySelectorAll('.card');
+  cards.forEach((card, y) => {
+    card.innerHTML = board[x][y];
+  });
+});
