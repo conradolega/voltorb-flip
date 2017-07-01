cdfc4caa277f7f79d850dcefce4ba433eaa27c8b
Conrad Olega <conradolega@gmail.com>
2017-07-01 19:09:43 +0800

    Compute score when selecting cards

--- game.js
+++ game.js
@@ -4,13 +4,23 @@ const board = Array(5).fill().map((el) => {
   return Array(5).fill().map((el) => values[Math.floor(4 * Math.random())]);
 });
 
+let score = 0;
+const scoreSpan = document.querySelector('.score');
+
 const rows = document.querySelectorAll('.grid .row');
 rows.forEach((row, x) => {
   const cards = row.querySelectorAll('.card');
   cards.forEach((card, y) => {
-    card.querySelector('.back').innerHTML = board[x][y];
+    const value = board[x][y];
+    card.querySelector('.back').innerHTML = value;
     card.querySelector('.front').addEventListener('click', (e) => {
       card.classList.add('flipped');
+      if (score === 0) {
+        score += value;
+      } else {
+        score *= value;
+      }
+      scoreSpan.innerHTML = score;
     });
   });
 });
