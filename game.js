5d8b5376397f16edad2c97d4b4a7bff49bdbfbf0
Conrad Olega <conradolega@gmail.com>
2017-07-02 16:07:44 +0800

    Refactor board generation into functions

--- game.js
+++ game.js
@@ -1,26 +1,31 @@
-const values = [0, 1, 2, 3];
+function generateBoardValues() {
+  const values = [0, 1, 2, 3];
+  return Array(5).fill().map((el) => {
+    return Array(5).fill().map((el) => values[Math.floor(4 * Math.random())]);
+  });
+}
 
-const board = Array(5).fill().map((el) => {
-  return Array(5).fill().map((el) => values[Math.floor(4 * Math.random())]);
-});
-
-let score = 0;
-const scoreSpan = document.querySelector('.score');
-
-const rows = document.querySelectorAll('.grid .row');
-rows.forEach((row, x) => {
-  const cards = row.querySelectorAll('.card');
-  cards.forEach((card, y) => {
-    const value = board[x][y];
-    card.querySelector('.back').innerHTML = value;
-    card.querySelector('.front').addEventListener('click', (e) => {
-      card.classList.add('flipped');
-      if (score === 0) {
-        score += value;
-      } else {
-        score *= value;
-      }
-      scoreSpan.innerHTML = score;
+function generateBoard() {
+  let score = 0;
+  const scoreSpan = document.querySelector('.score');
+  const boardValues = generateBoardValues();
+  const rows = document.querySelectorAll('.grid .row');
+  rows.forEach((row, x) => {
+    const cards = row.querySelectorAll('.card');
+    cards.forEach((card, y) => {
+      const value = boardValues[x][y];
+      card.querySelector('.back').innerHTML = value;
+      card.querySelector('.front').addEventListener('click', (e) => {
+        card.classList.add('flipped');
+        if (score === 0) {
+          score += value;
+        } else {
+          score *= value;
+        }
+        scoreSpan.innerHTML = score;
+      });
     });
   });
-});
+}
+
+generateBoard();
